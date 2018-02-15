3b50ced2d8e09a860593182e660c7e1d02a10afb
Matthew Planchant <matt@logiccache.co.uk>
2018-02-15 11:33:32 +0000

    Modified gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
-            baseDir: ''
+		proxy: "local.dev"
         },
     })
 })
